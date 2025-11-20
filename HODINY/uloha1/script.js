f7e94bd47414fefed8bc6691d729ac1b85f7d48f
LukasRous <lukasrous0@gmail.com>
2025-11-20 10:13:26 +0100

    xx

---
+++ HODINY/uloha1/script.js
@@ -0,0 +1,23 @@
+/* úloha Hello World 
+vypsat do konzole pomocí console.log
+*/
+
+/* console.log("konzole")
+console.error("error")
+console.warn("varovani")
+console.table(["hodnota1", "hodnota2"])
+ window.alert("###") */
+
+
+
+    let input1 = document.getElementById("input1").value
+    let input2 = document.getElementById("input2").value
+
+    let sum = 0
+
+    function calc() {
+        sum = input1 + input2
+        console.log(sum)
+    }
+
+  
